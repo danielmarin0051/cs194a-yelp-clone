0d227f810a01ccb4d1751a0f74171e42f0b9b1c7
Daniel Marin <danielmarin0051@gmail.com>
2021-11-11 23:01:00 -0800

    Add extension and readme

---
+++ README.md
@@ -0,0 +1,44 @@
+# Yelp Clone 
+
+## Daniel Marin
+
+**Yelp clone** displays a list of search results from the Yelp API and displays the results in a scrollable list. 
+
+Time spent: **3** hours spent in total
+
+## Functionality 
+
+The following **required** functionality is completed:
+
+* [x] Ability to query the Yelp API to get results from a search query
+* [x] The search results are displayed in a RecyclerView
+
+The following **extensions** are implemented:
+
+* [x] User sees a descriptive error message if internet is not available
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='https://media.giphy.com/media/lkwyZEZcqs98whDWjt/giphy.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+## Notes
+
+Didn't found any challenges.
+
+## License
+
+    Copyright 2021 Daniel Marin
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- app/src/main/java/com/danielmq/myyelpclone/MainActivity.kt
+++ app/src/main/java/com/danielmq/myyelpclone/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.danielmq.myyelpclone
 
+import android.content.Context
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,6 +12,15 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import android.net.NetworkInfo
+
+import android.net.ConnectivityManager
+import android.os.Build
+import android.view.LayoutInflater
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import java.io.IOException
+
 
 private const val TAG = "MainActivity"
 private const val BASE_URL = "https://api.yelp.com/v3/"
@@ -31,6 +42,7 @@ class MainActivity : AppCompatActivity() {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
         val yelpService =retrofit.create(YelpService::class.java)
+
         yelpService.searchRestaurants("BEARER $API_KEY","Avocado Toast", "New York").enqueue(object: Callback<YelpSearchResult> {
             override fun onResponse(call: Call<YelpSearchResult>, response: Response<YelpSearchResult>) {
                 Log.i(TAG, "onResponse $response");
@@ -45,9 +57,39 @@ class MainActivity : AppCompatActivity() {
 
             override fun onFailure(call: Call<YelpSearchResult>, t: Throwable) {
                 Log.i(TAG, "onFailure $t")
+                val isOnline = isOnline()
+                Log.i(TAG, "isOnline $isOnline")
+                if (!isOnline) {
+                    this@MainActivity.showAlertDialog()
+                }
+            }
+
+            private fun isOnline(): Boolean {
+                val runtime = Runtime.getRuntime()
+                try {
+                    val ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8")
+                    val exitValue = ipProcess.waitFor()
+                    return exitValue == 0
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+                return false
             }
 
         })
+    }
+    fun showAlertDialog() {
+        val placeFormView = LayoutInflater.from(this).inflate(R.layout.activity_main, null)
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("You are not connected to the internet!")
+            .setView(placeFormView)
+            .setPositiveButton("OK", null)
+            .show()
 
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
+            dialog.dismiss()
+        }
     }
 }
